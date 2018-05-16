d55d05940670d876a25287c7c09b3227e254e7c2
ycxj <1464151221@qq.com>
2018-05-16 14:57:09 +0800

    add vue-jd-app_img

--- js/projectData.js
+++ js/projectData.js
@@ -6,15 +6,16 @@ var project = [
 		"href":"demo/tmall/index.html"
 	},
 	{
-		"id":"1",
+		"id":"2",
 		"title":"放大镜特效",
 		"img":"magnifier.png",
 		"href":"demo/magnifier.html"
 	},
 	{
-		"id":"1",
-		"title":"客户管理系统",
-		"img":"6.cebf25e7.jpg"
+		"id":"3",
+		"title":"vue.js模仿京东商城移动端",
+		"img":"vue-jd-app.png",
+		"href": "demo/vue-jd-app/index.html"
 	},
 	{
 		"id":"1",
